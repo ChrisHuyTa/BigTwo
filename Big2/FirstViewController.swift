95699eac464e452987b0f4b79630312863cbdf0e
Christopher Ta <christa@macbook-pro-3.local>
2018-01-04 19:32:28 -0500

    Initial Commit

---
+++ Big2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Big2
+//
+//  Created by Christopher Ta on 2018-01-04.
+//  Copyright © 2018 ___tkl___. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
