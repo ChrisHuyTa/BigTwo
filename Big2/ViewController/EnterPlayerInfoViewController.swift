19c156af628671f733368c0ad26ed3c9a6528f46
Christopher Ta <christa@macbook-pro-3.local>
2018-01-07 21:17:18 -0500

    update

--- Big2/ViewController/EnterPlayerInfoViewController.swift
+++ Big2/ViewController/EnterPlayerInfoViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class EnterPlayerInfoViewController: UIViewController {
 
+    let playerContainerView: UIStackView = {
+        let sv = UIStackView()
+        sv.axis = .vertical
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        return sv
+    }()
+    
     let startButton: UIButton = {
         let btn = UIButton()
         btn.setTitle("Start New Game", for: .normal)
@@ -18,33 +25,49 @@ class EnterPlayerInfoViewController: UIViewController {
     }()
     
     let playerOneNameTextField: UITextField = {
-        let tf = UITextField()
+        let tf = UITextField(frame: CGRect(x:0, y:0, width:30, height:30))
+        tf.placeholder = "Player 1 Name"
         tf.translatesAutoresizingMaskIntoConstraints = false
         return tf
     }()
     
     let playerTwoNameTextField: UITextField = {
         let tf = UITextField()
+        tf.placeholder = "Player 2 Name"
         tf.translatesAutoresizingMaskIntoConstraints = false
         return tf
     }()
     
     let playerThreeNameTextField: UITextField = {
         let tf = UITextField()
+        tf.placeholder = "Player 3 Name"
         tf.translatesAutoresizingMaskIntoConstraints = false
         return tf
     }()
     
     let playerFourNameTextField: UITextField = {
         let tf = UITextField()
+        tf.placeholder = "Player 4 Name"
         tf.translatesAutoresizingMaskIntoConstraints = false
         return tf
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.title = "Game"
+        
+        self.view.addSubview(playerContainerView)
+        
+        playerContainerView.addArrangedSubview(playerOneNameTextField)
+        playerContainerView.addArrangedSubview(playerTwoNameTextField)
+        playerContainerView.addArrangedSubview(playerThreeNameTextField)
+        playerContainerView.addArrangedSubview(playerFourNameTextField)
+        
+        playerContainerView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        playerContainerView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -100)
+        playerContainerView.widthAnchor.constraint(equalToConstant: 300)
+        playerContainerView.heightAnchor.constraint(equalToConstant: 300)
+        
         
     }
 
